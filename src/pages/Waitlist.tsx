9f079324c989cfbae8489f5cbc687f9992de6f55
bndambuki123 <bndambuki123@gmail.com>
2025-05-27 12:01:00 +0300

    Set up Supabase connection and form submission

--- src/pages/Waitlist.tsx
+++ src/pages/Waitlist.tsx
@@ -463,8 +463,8 @@ const FAQItem: React.FC<FAQItemProps> = ({ question, answer }) => {
         <span>{question}</span>
         <span className="ml-4">
           {isOpen ? (
-            <svg className="w-5 h-5 text-primary-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+            <svg className="w-5 h-5 text-primary-600\" fill="none\" stroke="currentColor\" viewBox="0 0 24 24\" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round\" strokeLinejoin="round\" strokeWidth="2\" d="M19 9l-7 7-7-7"></path>
             </svg>
           ) : (
             <svg className="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
